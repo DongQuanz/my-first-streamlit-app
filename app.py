932ed7837912cf094df9fec86524873446d3e0d2
DongQuanz <nndquan296@gmail.com>
2023-03-02 22:59:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("Congratulations!")
-st.header("You've just created a website")
-st.balloons()
-st.snow()
+x = 6
+st.write('Hello COTAI', x)
+st.title('This is a title')
+st.header('This is a header')
+st.subheader('This is a subheader')
+st.caption('This is a caption')
+st.text('This is a text')
+st.code('x = 5\nprint(x)')
+st.latex('x^2 + \sqrt{y} = \pi')
